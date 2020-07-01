825d7db38f87e1fa5018ab1c3c8e1d9cfc05238f
little_whale <2786435349@qq.com>
2020-07-01 15:18:42 +0800

    完成商品功能开发

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,8 @@ import Rights from '../components/power/Rights'
 import Roles from '../components/power/Roles'
 import Cate from '../components/goods/Cate'
 import Param from '../components/goods/Params'
+import List from '../components/goods/List'
+import Add from '../components/goods/Add'
 
 Vue.use(VueRouter)
 
@@ -24,7 +26,9 @@ const router = new VueRouter({
         { path: '/rights', component: Rights },
         { path: '/roles', component: Roles },
         { path: '/categories', component: Cate },
-        { path: '/params', component: Param }]
+        { path: '/params', component: Param },
+        { path: '/goods', component: List },
+        { path: '/goods/add', component: Add }]
     }
   ]
 })
