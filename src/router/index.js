30a2e80de077e12902aee25d2e7c870746d7e305
little_whale <2786435349@qq.com>
2020-07-01 16:19:16 +0800

    完成订单功能

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,7 @@ import Cate from '../components/goods/Cate'
 import Param from '../components/goods/Params'
 import List from '../components/goods/List'
 import Add from '../components/goods/Add'
+import Order from '../components/order/Order'
 
 Vue.use(VueRouter)
 
@@ -28,7 +29,8 @@ const router = new VueRouter({
         { path: '/categories', component: Cate },
         { path: '/params', component: Param },
         { path: '/goods', component: List },
-        { path: '/goods/add', component: Add }]
+        { path: '/goods/add', component: Add },
+        { path: '/orders', component: Order }]
     }
   ]
 })
